b9ad79e2419cbaaca214c426e362ae5044f2f347
biogui <gmrtri@hotmail.com>
2020-08-31 12:44:31 -0300

    .

--- ICC/ICC.I/Trabalhos/SGBD/Codes/busca.h
+++
@@ -1,9 +0,0 @@
-// Biblioteca com as funções centrais para busca de fichas no banco de dados
-#ifndef BUSCA_H
-#define BUSCA_H
-
-#include "metadados.h"
-
-int buscaFichaEImprimeFicha(Metadados*, char*);
-
-#endif
--- ICC/ICC.I/Trabalhos/SGBD/Codes/insercao.h
+++
@@ -1,9 +0,0 @@
-// Biblioteca com as funções centrais para inserção de novas fichas no banco de dados
-#ifndef INSERCAO_H
-#define INSERCAO_H
-
-#include "metadados.h"
-
-int recebeDadosReg(Dados*);
-
-#endif
